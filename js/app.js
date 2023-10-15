ac6632f8688c537950691d69b17a6255d2d02f99
Yeison Estiven Perea Córdoba <99289539+perea07@users.noreply.github.com>
2023-10-15 09:54:09 -0500

    Actualización en el código

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
 // Oculta el efecto de carga y muestra el contenido después de cargar los productos
 async function fetchProducts() {
-
     try {
         const response = await fetch('https://api-products-spring-boot-production.up.railway.app/api/list-products');
         const data = await response.json();
@@ -14,7 +13,10 @@ async function fetchProducts() {
                 <div class="card product-card">
                     <div>
                         <img src="${product.img}" class="card-img-top img-size" alt="${product.name}">
+                        <center>
+                        <div class="py-3"><i class="fa-solid fa-star"></i><i class="fa-regular fa-star"></i><i class="fa-regular fa-star"></i><i class="fa-regular fa-star"></i><i class="fa-regular fa-star"></i></div>
                     </div>
+                    </center>
                     <div class="card-body">
                         <h5 class="card-title product-title">${product.name}</h5>
                         <p class="card-text product-price">Price: $${product.price}</p>
@@ -25,6 +27,7 @@ async function fetchProducts() {
                         </button></div>
                     </div>
                     <a href="#" class="text-warning px-5" data-toggle="modal" data-target="#myModal">Ver detalles</a>
+                    <button type="button" class="btn btn-warning"><i class="fa-solid fa-cart-plus"></i></button>
                 </div>
             </div>
         `;
